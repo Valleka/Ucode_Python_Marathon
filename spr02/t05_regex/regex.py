54d4465e3be74523090f2c3f6bbff76cedf3e6db
Валерій <valerij@iMac-Danilo.local>
2021-04-20 01:53:11 +0300

    sp02_t05

--- spr02/t05_regex/regex.py
+++ spr02/t05_regex/regex.py
@@ -1,6 +1,6 @@
 import re
 
-def check_address2(args):
+def check_address(args):
     buf = r'^Ukraine,[ ]*[A-Za-z-[ ]*]*,[ ]*[A-Za-z-[ ]*]*[ ]*\d{1,6},[ ]*\d{5}$'
     res = []
     for i in args:
@@ -8,16 +8,4 @@ def check_address2(args):
             res.append(f"The address of {i} is valid.")
         else:
             res.append(f"The address of {i} is invalid.")
-    return res
-
-
-
-def check_address(dict):
-    tpl = r'^Ukraine,[ ]*[A-Za-z-[ ]*]*,[ ]*[A-Za-z-[ ]*]*[ ]*\d{1,6},[ ]*\d{5}$'
-    res = []
-    for it in dict:
-        if re.match(tpl, dict[it]) is not None:
-            res.append(f'The address of {it} is valid.')
-        else:
-            res.append(f'The address of {it} is invalid.')
     return res
